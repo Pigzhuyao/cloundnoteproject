ad788e21ff72137175ecb630463d557450104fcd
zhuyao <zhuyao@g-soft.com.cn>
2022-12-30 09:53:57 +0800

    Init router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router' // zy: can take alias
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router, // zy: Or router: router's alias
   components: { App },
   template: '<App/>'
 })
